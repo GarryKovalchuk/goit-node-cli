99ada0b0688995a7fbb96493d8accbe954f0581b
Garry <vorzel47051@gmail.com>
2024-03-08 21:31:45 +0200

    NODE01

---
+++ contacts.js
@@ -0,0 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("db", "contacts.json");
+console.log(contactsPath);
+
+async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
